9571e6625691858c38ee22682d0335c9301e278b
jovaine <70172902+jovaine@users.noreply.github.com>
2020-08-31 16:22:54 -0400

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 50;
+	float f = 24.99f;
+	String s = "Bobby Johnson";
+	System.out.println("Test Score initialized is: "+i);
+	System.out.println("Price tag: "+f);
+	System.out.println("Username: "+s);
+
+    }
+}
